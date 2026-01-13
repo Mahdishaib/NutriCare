5f0d9fbd59d8769ce8f3d8d04f1d47f64be7f937
Mahdishaib <124410102+Mahdishaib@users.noreply.github.com>
2026-01-14 00:19:53 +0200

    emptyy

--- Message.js
+++ Message.js
@@ -1,11 +1,1 @@
-const mongoose = require('mongoose');
 
-const MessageSchema = new mongoose.Schema({
-  id: { type: Number, unique: true },
-  senderId: Number,
-  receiverId: Number,
-  text: String,
-  timestamp: String
-});
-
-module.exports = mongoose.model('Message', MessageSchema);
